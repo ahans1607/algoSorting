ded1107c49c5574ed48de303b02b70d3985eab02
Ananya Hans <77026617+ahans1607@users.noreply.github.com>
2021-08-31 23:37:25 -0500

    Update 05-quick-sort.js

--- problems/05-quick-sort.js
+++ problems/05-quick-sort.js
@@ -1,7 +1,37 @@
 // Implement Quick Sort
 
 function quickSort(array) {
-  // your code here
+  // Creating an array that we'll use as a stack, using the push() and pop() functions
+    stack = [];
+    
+    // Adding the entire initial array as an "unsorted subarray"
+    stack.push(0);
+    stack.push(arr.length - 1);
+    
+    // There isn't an explicit peek() function
+    // The loop repeats as long as we have unsorted subarrays
+    while(stack[stack.length - 1] >= 0){
+        
+        // Extracting the top unsorted subarray
+    	end = stack.pop();
+        start = stack.pop();
+        
+        pivotIndex = partition(arr, start, end);
+        
+        // If there are unsorted elements to the "left" of the pivot,
+        // we add that subarray to the stack so we can sort it later
+        if (pivotIndex - 1 > start){
+        	stack.push(start);
+            stack.push(pivotIndex - 1);
+		}
+        
+        // If there are unsorted elements to the "right" of the pivot,
+        // we add that subarray to the stack so we can sort it later
+        if (pivotIndex + 1 < end){
+        	stack.push(pivotIndex + 1);
+            stack.push(end);
+        }
+    }
 }
 
 
