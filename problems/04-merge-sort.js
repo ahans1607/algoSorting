9d6533bbe8529d321bc793893ebeddffb8003ce7
Ananya Hans <77026617+ahans1607@users.noreply.github.com>
2021-08-31 23:36:12 -0500

    Update 04-merge-sort.js

--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -1,7 +1,20 @@
 // Implement Merge Sort
 
-function merge(array1, array2) {
-  // your code here
+function merge(left, right) {
+  let arr = []
+    // Break out of loop if any one of the array gets empty
+    while (left.length && right.length) {
+        // Pick the smaller among the smallest element of left and right sub arrays 
+        if (left[0] < right[0]) {
+            arr.push(left.shift())  
+        } else {
+            arr.push(right.shift()) 
+        }
+    }
+    
+    // Concatenating the leftover elements
+    // (in case we didn't go through the entire left or right array)
+    return [ ...arr, ...left, ...right ]
 }
 
 function mergeSort(array) {
