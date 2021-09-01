52a1e0f910f3f7d5df484029ba82b790f38eeee1
Ananya Hans <77026617+ahans1607@users.noreply.github.com>
2021-08-31 23:36:26 -0500

    Update 04-merge-sort.js

--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -18,7 +18,15 @@ function merge(left, right) {
 }
 
 function mergeSort(array) {
-  // your code here
+   const half = array.length / 2
+  
+  // Base case or terminating case
+  if(array.length < 2){
+    return array 
+  }
+  
+  const left = array.splice(0, half)
+  return merge(mergeSort(left),mergeSort(array))
 }
 
 module.exports = {
